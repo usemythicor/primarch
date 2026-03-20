1bb7eba776bd2e949c5c37ae35676091c652ace9
FounderCasey <caseywilcox@outlook.com>
2026-03-20 16:08:55 -0500

    refactor: always hide console window on Windows
    Remove debug_assertions condition to hide console in both debug and
    release builds.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents additional console window on Windows, DO NOT REMOVE!!
+#![windows_subsystem = "windows"]
 
 fn main() {
     primarch_lib::run()
